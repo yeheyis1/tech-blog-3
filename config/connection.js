6f8e47f561ee5ef5f906b9bb81c70eb129c514b8
Yeheyis Getachew <68479765+yeheyis1@users.noreply.github.com>
2024-09-30 21:40:24 -0400

    fix env variable

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 let sequelize;
 
-if (process.env.DATABASE_URL) {
+if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL)
 } else {
   sequelize = new Sequelize(
